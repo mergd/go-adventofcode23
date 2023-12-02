2d739450e4f95b19c4487533b01646e0d15107bc
mergd <24722014+mergd@users.noreply.github.com>
2023-12-02 15:06:03 -0800

    initial commit

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func calculateLine(line string) int {
 	var firstInt int
 	var lastInt int
+	// grep every mention of number with int
+
 	for _, char := range strings.Split(line, "") {
 		i, err := strconv.ParseInt(char, 10, 64)
 		if err != nil {
@@ -31,6 +33,14 @@ func calculateLine(line string) int {
 }
 
 func main() {
+
+	type Tuple struct {
+		str string
+		num int
+	}
+
+	var strs = []Tuple{{"one", 1}, {"two", 2}, {"three", 3}, {"four", 4}, {"five", 5}, {"six", 6}, {"seven", 7}, {"eight", 8}, {"nine", 9}}
+
 	var runningTotal int = 0
 	puzzleData, fsError := os.ReadFile("file.txt")
 	if fsError != nil {
@@ -40,7 +50,16 @@ func main() {
 
 	puzzleDataString := string(puzzleData)
 
-	for _, line := range strings.Split(puzzleDataString, "\n") {
+	for _, num := range strs {
+		puzzleDataString = strings.Replace(puzzleDataString, num.str, strconv.Itoa(num.num), -1)
+
+	}
+
+	for iterator, line := range strings.Split(puzzleDataString, "\n") {
+		if iterator > 10 {
+
+			print(line)
+		}
 		runningTotal += calculateLine(line)
 	}
 
